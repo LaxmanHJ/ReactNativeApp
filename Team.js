dfc04f3d6f0eebb746713ec78aede92a9b793aa5
Lucky <51163246+LaxmanHJ@users.noreply.github.com>
2021-01-12 01:01:46 +0530

    added some ui to TeamPage

--- Team.js
+++ Team.js
@@ -11,69 +11,125 @@ export default class TeamScreen extends React.Component {
     modalVisible:false, 
     setModalVisible:false,
     searchTeamValue:'',
+    teamName:'',
+    teamLocation:'',
+    teamLevel:'',
+    teamId:'',
+    userId:'',
+  }
+  componentDidMount() {
+    this._loadInitialState().done();
   }
 
-  render() { 
+  // AsyncStorage for login data processing
+  _loadInitialState=async()=>{
+    var phone = await AsyncStorage.getItem('phone');
+    this.setState({userId:phone})
+    this.teamDetails();
+  }
 
+  teamDetails=()=>{
+    console.log("Ranking algo is running");
+    const data=this.state.userId;
+    //const data = '7389';
+    let endpoint = `http://localhost:8080/piedpiper/TeamRequest.php?phone=${encodeURIComponent(data)}`;
+    fetch(endpoint, {
+                method: 'GET',
+                
+            })
+          .then((response) => response.json())
+            .then((responseData) => {
+                                    console.log(responseData);
+                                    console.log(typeof(responseData.TeamId));
+                                    if (responseData.msg=='success'){
+                                    this.setState({
+                                      teamName:responseData.name,
+                                      teamLocation:responseData.Location,
+                                      teamLevel:responseData.level,
+                                      teamId:responseData.TeamId,
+                                    })
+                                  }else if(responseData.msg="NoTeamFound"){
+                                    console.log(responseData.msg);
+                                    this.setState({
+                                    teamId:0,
+                                    })
+
+                                  }
+                                   
+            }).catch((error)=> {
+              console.error("Error Message 1"+error)
+            });
+      }
+  render() { 
+    const isTeam = parseInt(this.state.TeamId);
     return (  
       <View style={styles.container}> 
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={this.state.modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            
-            <TouchableHighlight
-              style={styles.openButton }
-              onPress={() => {
-                //this.state.setModalVisible(!this.state.modalVisible);
-                this.setState({modalVisible:!this.state.modalVisible});          
-
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={this.state.modalVisible}
+              onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
               }}
             >
-              <View style={styles.gridChild}>
-                  <Text style={styles.modalText}>Cancel</Text>
-                  <Text style={styles.modalText}>Create Team</Text>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                  <TouchableHighlight
+                    style={styles.openButton }
+                    onPress={() => {
+                      //this.state.setModalVisible(!this.state.modalVisible);
+                      this.setState({modalVisible:!this.state.modalVisible});          
+
+                    }}
+                  >
+                    <View style={styles.gridChild}>
+                        <Text style={styles.modalText}>Cancel</Text>
+                        <Text style={styles.modalText}>Create Team</Text>
+                    </View>
+
+                  </TouchableHighlight>
+
+                  <View style={styles.inputView} >
+                    <TextInput  
+                      style={styles.input}
+                      placeholder="Search Team..." 
+                      placeholderTextColor="black"
+                      onChangeText={searchTeamValue => this.setState({searchTeamValue})}/>
+                  </View>
+                    <Text style={styles.modalText}>{this.state.searchTeamValue} Team</Text>
+                </View>
               </View>
-
-            </TouchableHighlight>
-
-          <View style={styles.inputView} >
-            <TextInput  
-              style={styles.input}
-              placeholder="Search Team..." 
-              placeholderTextColor="black"
-              onChangeText={searchTeamValue => this.setState({searchTeamValue})}/>
-          </View>
-            <Text style={styles.modalText}>{this.state.searchTeamValue} Team</Text>
-
-       
-
-
-          </View>
-        </View>
-      </Modal>
+            </Modal>
 
             <View style={styles.gridChild}>
                   <View style={styles.header}>
                         <Text style={styles.logo}>Team</Text>
                   </View>
                   <View style={styles.Iconsetter}>
-                  <TouchableHighlight
-                  onPress={() => {
-                    this.setState({modalVisible:true});          
-                    // Alert.alert("Modal has been opened.");
-                }}>
-
-                    <Icon style={[{color: 'coral'}]} size={30} name={'ios-create'}/>
+                    <TouchableHighlight
+                        onPress={() => {
+                          this.setState({modalVisible:true});          
+                          // Alert.alert("Modal has been opened.");
+                      }}>
+                     <Icon style={[{color: 'coral'}]} size={30} name={'ios-create'}/>
                     </TouchableHighlight>
                   </View> 
             </View>
+
+            <View >
+                  {(isTeam) != 0
+                      ? 
+                    <View style={styles.TeamView}>
+                      <Text style={styles.TeamViewHeaderFont}>{this.state.teamName}</Text>
+                      <Text>Level                       {this.state.teamLevel}</Text>
+                      <Text>Location                    {this.state.teamLocation}</Text>
+
+                    </View>
+                     : 
+                     <Text>No Team to Display</Text>
+                }
+               
+            </View>     
         </View>
     );  
 }  
@@ -84,7 +140,15 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 40,
     padding:2  
+  },
+  TeamView:{
+    padding:10,
+    backgroundColor:'#c0c0c0'
   }, 
+  TeamViewHeaderFont:{
+    fontSize:30,
+
+  },
   logo:{
     fontWeight:"bold",
     fontSize:40,
@@ -154,6 +218,6 @@ const styles = StyleSheet.create({
   },
   input:{
     padding:9,
-  }
+  },
  
 });  
